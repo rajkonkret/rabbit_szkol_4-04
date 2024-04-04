7bd331f6a982ed7ceeba531a011c5634803dc92d
rajkonkret <rajkonkret660@gmail.com>
2024-04-04 14:14:17 +0200

    add TestPublisher

---
+++ src/main/java/org/example/TestPublisher.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class TestPublisher {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection(CommonConfig.AMQP_URL);
+        Channel channel = connection.createChannel();
+
+//        String message = "Turn on home appliances";
+        String message = "Turn on personal";
+//        channel.basicPublish("my-direct-exchange", "homeAppliance",null,  message.getBytes());
+        channel.basicPublish("my-direct-exchange", "personalDevice",null,  message.getBytes());
+
+        channel.close();
+        connection.close();
+    }
+}
