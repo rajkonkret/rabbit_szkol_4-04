58075612a5e2e8dd60d520ce613e81f744b8c82d
rajkonkret <rajkonkret660@gmail.com>
2024-04-04 14:25:54 +0200

    add TestConsumer

---
+++ src/main/java/org/example/TestConsumer.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class TestConsumer {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection connection = factory.newConnection(CommonConfig.AMQP_URL);
+        Channel channel = connection.createChannel();
+
+        DeliverCallback deliverCallback = (consumerTag, message) -> {
+            System.out.println(consumerTag);
+            System.out.println(new String(message.getBody(), "UTF-8"));
+        };
+
+        CancelCallback cancelCallback = consumerTag -> {
+            System.out.println(consumerTag);
+        };
+//        channel.basicConsume("ACQ", true, deliverCallback, cancelCallback);
+//        channel.basicConsume("LightQ", true, deliverCallback, cancelCallback);
+        channel.basicConsume("MobileQ", true, deliverCallback, cancelCallback);
+    }
+}
