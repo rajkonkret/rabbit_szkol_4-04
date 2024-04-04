7c5a16bfca591bdeefb64a713acc7bbb238210f6
rajkonkret <rajkonkret660@gmail.com>
2024-04-04 11:55:57 +0200

    add conection to rabbit server and create first queue and publish first message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class Main {
+    private static final String QUEUE_NAME = "hello";
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello, RabbitMQ";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [X] Sent'" + message + "'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
